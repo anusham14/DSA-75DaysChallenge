4324ea4838cefa162227397d09389f6ed6a79e3d
anusham14 <55886250+anusham14@users.noreply.github.com>
2024-03-20 19:50:00 +0530

    Create mergeSorted.java

---
+++ mergeSorted.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/merge-sorted-array/
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i=0,j=0,k=0;
+        int[] ans = new int[m+n];
+        while(i<m && j<n){
+            if(nums1[i]<=nums2[j]){
+                ans[k] = nums1[i];
+                i++;
+            }
+            else{
+                ans[k] = nums2[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<m){
+            ans[k] = nums1[i];
+                i++;
+                k++;
+        }
+        while(j<n){
+            ans[k] = nums2[j];
+                j++;
+                k++;
+        }
+        for(int a=0;a<m+n;a++){
+            nums1[a] = ans[a];
+        }
+    }
+}

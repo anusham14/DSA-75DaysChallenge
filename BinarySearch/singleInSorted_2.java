32dd356a520785482f3d1c95b0bcac3e3af6631e
anusham14 <55886250+anusham14@users.noreply.github.com>
2024-07-30 05:17:28 +0530

    Create singleInSorted_2.java

---
+++ BinarySearch/singleInSorted_2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+
+        int n = nums.length;
+       int start = 1 , end = n-2;
+       if(n == 1) return nums[0];
+       if(nums[0] != nums[1]) return nums[0];
+       if(nums[n-1] != nums[n-2]) return nums[n-1];
+
+       while(start <= end){
+        int mid = end + (start-end)/2 ;
+        if(nums[mid]!= nums[mid+1] && nums[mid]!= nums[mid-1]) 
+            return nums[mid];
+        if((mid % 2 == 0 && nums[mid] == nums[mid+1]) || (mid % 2 != 0 && nums[mid] == nums[mid-1])) start = mid+1;
+       else end = mid-1;
+    }
+    return -1;
+    }
+}
